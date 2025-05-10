147fe380876e4ff7f2c3718cd43ddb336eebd5a1
Vinsengi <nvip2020@gmail.com>
2025-05-10 22:54:43 +0200

    Fix venv errors

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,5 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
-
-
+    excerpt = models.TextField(max_length=100, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
