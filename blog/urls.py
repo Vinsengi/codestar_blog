35edc1c0e7e983c510301a2b199254b23324e9bc
Vinsengi <nvip2020@gmail.com>
2025-05-15 20:48:25 +0200

    create generic post view

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from .import views
+from django.urls import path
+from .views import PostList
+
+
+urlpatterns = [
+    path('', PostList.as_view(), name="home"),
+  
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render # type: ignore
+from django.views import generic
+from .models import Post
 
 #  Create your views here.
-
-
-def my_blog(request):
-    return HttpResponse("Hello, blog, vital here!")
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
