4d7ca854cd6f2bacdf2c7caef455d20747e76757
Vinsengi <nvip2020@gmail.com>
2025-05-15 22:11:13 +0200

    Install summernote and update database model and work on the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment   
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
+    list_display = ("title", "slug", "author", "status")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "created_on"
+    ordering = ("status", "created_on")
 
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
