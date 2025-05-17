720ff00b9377f71ac1e876c205605a064f17f26e
Vinsengi <nvip2020@gmail.com>
2025-05-17 10:42:10 +0200

    Create static file, top level base html and app level index html

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
+    
